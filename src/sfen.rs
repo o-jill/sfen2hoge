54245937ca2e97885da218b241dc2d487091f8ad
o-jill <nobaokix@gmail.com>
2022-01-29 22:39:08 +0900

    added reading tegoma.

--- src/sfen.rs
+++ src/sfen.rs
@@ -11,7 +11,7 @@ pub struct Sfen {
     nteme: i32,
 }
 
-fn extractdan(txt: &str) -> String {
+fn extractdan(txt: &str) -> Result<String, String> {
     let mut res = String::from("|");
     let masu = txt.chars();
     let mut promote = ' ';
@@ -34,10 +34,38 @@ fn extractdan(txt: &str) -> String {
                 res = res + &promote.to_string() + &ch.to_string();
                 promote = ' ';
             }
-            _ => res = res + &ch.to_string(),
+            _ => return Err(format!("{} is not allowed to use!!", ch)),
         }
     }
-    res + "|"
+    Ok(res + "|")
+}
+
+fn extracttegoma(txt: &str) -> Result<(String, String), String> {
+    let resente = Regex::new("[PLNSGBRK]").unwrap();
+    let regote = Regex::new("[plnsgbrk]").unwrap();
+    let mut sentegoma = String::new();
+    let mut gotegoma = String::new();
+    let mut num = 0;
+    for ch in txt.chars() {
+        match ch {
+            '1'..='9' => num = num * 10 + ch.to_digit(10).unwrap(),
+            ch if resente.is_match(&ch.to_string()) => {
+                if num == 0 {
+                    num = 1
+                };
+                sentegoma = sentegoma + &String::from_utf8(vec![ch as u8; num as usize]).unwrap();
+                num = 0;
+            }
+            ch if regote.is_match(&ch.to_string()) => {
+                gotegoma = gotegoma + &String::from_utf8(vec![ch as u8; num as usize]).unwrap();
+
+                num = 0;
+            }
+            '-' => return Ok((String::new(), String::new())),
+            _ => return Err(format!("{} is not allowed to use!!", ch)),
+        }
+    }
+    Ok((sentegoma, gotegoma))
 }
 
 impl Sfen {
@@ -50,12 +78,19 @@ impl Sfen {
             nteme: e[3].parse().unwrap_or(-1),
         }
     }
-
     pub fn dump(&self) -> String {
         let mut res = String::new();
         let vdan: Vec<&str> = self.ban.split("/").collect();
         for e in vdan {
-            res = res + &extractdan(e) + "\n";
+            match extractdan(e) {
+                Ok(ret) => res = res + &ret + "\n",
+                Err(msg) => return format!("error in [{}]:{}", e, msg),
+            }
+        }
+        match extracttegoma(&self.tegoma) {
+            Ok((sentegoma, gotegoma)) =>
+            res = format!("gote:{}\n{}sente:{}\n", gotegoma, res, sentegoma),
+            Err(msg) => return format!("error in [{}]:{}", self.tegoma, msg),
         }
         res
     }
