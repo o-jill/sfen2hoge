5dbf6dd27cc533439e1d1e386df8a503ccd77455
o-jill <nobaokix@gmail.com>
2022-02-03 13:05:22 +0900

    fixed kanji expression for tegoma.

--- src/sfen.rs
+++ src/sfen.rs
@@ -56,8 +56,9 @@ fn extractdan(txt: &str) -> Result<String, String> {
 
 fn kanjinum(num: usize) -> Result<String, String> {
     let kanji = [
-        "", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三",
-        "十四", "十五", "十六", "十七", "十八",
+        "", "", /*"一"*/
+        "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三", "十四",
+        "十五", "十六", "十七", "十八",
     ];
     if num > 18 {
         return Err(String::from("??"));
