896c36e9d446a6875bd545d295f28936d39cbfdf
o-jill <nobaokix@gmail.com>
2022-02-06 21:55:54 +0900

    supported launching with args.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,50 @@ use std::io;
 
 mod sfen;
 
+enum Mode {
+    Text,
+    SVG,
+    PNG,
+}
+
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
     // let txt = "9/9/9/9/9/9/9/9/9 w - 0";
     // let txt = "lnsgkgsnl/1r5b1/p1ppppp1p/9/9/9/P1PPPPP1P/1B2K2R1/LNSG1GSNL w 2P2p 2";
-    let txt = "ln+sgkgsnl/1+r5b1/p1pp+ppp1p/9/9/9/P1PPP+PP1+P/1+B2K2R1/LNS+G1GSNL w 2P2p 2";
+    let mut txt = "ln+sgkgsnl/1+r5b1/p1pp+ppp1p/9/9/9/P1PPP+PP1+P/1+B2K2R1/LNS+G1GSNL w 2P2p 2";
     // let txt = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1";
+
+    let mut md = Mode::Text;
+    for e in args.iter() {
+        if e == "--svg" {
+            md = Mode::SVG;
+        } else if e == "--png" {
+            md = Mode::PNG;
+        } else if e == "--txt" {
+            md = Mode::Text;
+        } else if e == "--help" {
+            println!("sfen2reader sfen [options]");
+            println!("sfen:");
+            println!("ex.\t\"lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1\"");
+            println!("options:");
+            println!("\t--txt  : text style.");
+            println!("\t--svg  : svg style.");
+            println!("\t--png  : png style.");
+            println!("\t--help : show this help.");
+            return;
+        } else {
+            txt = e;
+        }
+    }
+
     let sfen = sfen::Sfen::new(txt);
 
-    println!("sfen:{}", txt);
-    println!("{}", sfen.dump());
+    match md {
+        Mode::SVG => println!("svg will be here."),
+        Mode::PNG => println!("png will be here."),
+        _ => {
+            println!("sfen:{}", txt);
+            println!("{}", sfen.dump());
+        }
+    }
 }
